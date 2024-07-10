83389ca9e1ba8eda11a5471c52618fcf4e09e634
Md. Rafat Hossain <rafathossain96@gmail.com>
2024-07-11 00:31:05 +0600

    :rocket: Deployment Update

--- CGPA_Calculator/settings.py
+++ CGPA_Calculator/settings.py
@@ -11,19 +11,28 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '61+hl)l-f8d32y(df$*l3y1rgq0uh(e%l$w@1$ai5lkohz&ca*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
